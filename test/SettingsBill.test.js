3a1b9f5b53c774445b7edbf2fe6333c69de7c4e6
Pumlani <lani.buhle@gmail.com>
2018-05-14 15:01:08 +0200

    tested warning and critical level

--- test/SettingsBill.test.js
+++ test/SettingsBill.test.js
@@ -1,55 +1,104 @@
-describe('The settings-bill function logic',function(){
+describe('The settings-bill function logic', function() {
 
-  it('should calculate the total for two call', function(){
+  it('should calculate the total for two call', function() {
 
-    var FactoryObj = SettingsBill();
+    var factoryObj = SettingsBill();
 
-    FactoryObj.setCall(5);
-    FactoryObj.setSms(2.50);
-    FactoryObj.Warning();
-    FactoryObj.Critical();
+    factoryObj.setCall(5);
+    factoryObj.setSms(2.50);
+    factoryObj.Warning();
+    factoryObj.Critical();
 
 
 
-    FactoryObj.calculation('call');
-    FactoryObj.calculation('call');
+    factoryObj.calculation('call');
+    factoryObj.calculation('call');
 
-    assert.equal(FactoryObj.call(), 10);
-    assert.equal(FactoryObj.sms(), 0);
-    assert.equal(FactoryObj.total(), 10);
+    assert.equal(factoryObj.call(), 10);
+    assert.equal(factoryObj.sms(), 0);
+    assert.equal(factoryObj.total(), 10);
 
   });
 
-  it('should calculate the total for a call and sms', function(){
+  it('should calculate the total for a call and sms', function() {
 
-      var FactoryObj = SettingsBill();
+    var factoryObj = SettingsBill();
 
-      FactoryObj.setCall(5);
-      FactoryObj.setSms(2.50);
-        FactoryObj.Warning();
-          FactoryObj.Critical();
+    factoryObj.setCall(5);
+    factoryObj.setSms(2.50);
+    factoryObj.Warning();
+    factoryObj.Critical();
 
-      FactoryObj.calculation('call');
-      FactoryObj.calculation('sms');
+    factoryObj.calculation('call');
+    factoryObj.calculation('sms');
 
-      assert.equal(FactoryObj.call(), 5);
-      assert.equal(FactoryObj.sms(), 2.50);
-      assert.equal(FactoryObj.total(), 7.50);
+    assert.equal(factoryObj.call(), 5);
+    assert.equal(factoryObj.sms(), 2.50);
+    assert.equal(factoryObj.total(), 7.50);
 
-    });
-    it('Should make sure the total is lower or equal to the critical value', function () {
-
-        var FactoryObj = SettingsBill();
-
-        FactoryObj.setCall('5');
-        FactoryObj.setSms('2');
-        FactoryObj.Critical('10');
-        FactoryObj.calculation('call');
-        FactoryObj.calculation('call');
-
-        assert.isAtMost(FactoryObj.total(), FactoryObj.getCritical(),'toal is less or equal to critical value')
-
-
-
-    });
   });
+  it('Should make sure the total is lower or equal to the critical value', function() {
+
+    var factoryObj = SettingsBill();
+
+    factoryObj.setCall('5');
+    factoryObj.setSms('2');
+    factoryObj.Critical('10');
+
+    factoryObj.calculation('call');
+    assert.equal(false, factoryObj.hasReachedCriticalLevel());
+
+    factoryObj.calculation('call');
+    factoryObj.calculation('call');
+    factoryObj.calculation('sms');
+    factoryObj.calculation('call');
+    factoryObj.calculation('call');
+    factoryObj.calculation('sms');
+    factoryObj.calculation('call');
+    factoryObj.calculation('call');
+    factoryObj.calculation('sms');
+    factoryObj.calculation('call');
+    factoryObj.calculation('call');
+    factoryObj.calculation('sms');
+
+console.log(factoryObj.total());
+    assert.equal(true, factoryObj.hasReachedCriticalLevel());
+    assert.equal(10, factoryObj.total());
+
+
+    // assert.isAtMost(factoryObj.total(), factoryObj.getCritical(), 'total is less or equal to critical value')
+
+  });
+  it('Should change the text colour to orange', function() {
+
+    var factoryObj = SettingsBill();
+
+    factoryObj.setCall('5');
+    factoryObj.setSms('2');
+    factoryObj.Warning('20');
+    factoryObj.Critical('50');
+
+    factoryObj.calculation('call');
+    assert.equal(false, factoryObj.hasReachedWarningLevel());
+
+    factoryObj.calculation('call');
+    factoryObj.calculation('call');
+    factoryObj.calculation('sms');
+    factoryObj.calculation('call');
+    factoryObj.calculation('call');
+    factoryObj.calculation('sms');
+    factoryObj.calculation('call');
+    factoryObj.calculation('call');
+    factoryObj.calculation('sms');
+    factoryObj.calculation('call');
+    factoryObj.calculation('call');
+    factoryObj.calculation('sms');
+
+    assert.equal(true, factoryObj.hasReachedWarningLevel());
+    //assert.equal(20, factoryObj.total());
+
+
+    // assert.isAtMost(factoryObj.total(), factoryObj.getCritical(), 'total is less or equal to critical value')
+
+  });
+});
